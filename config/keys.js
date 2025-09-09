ddfa81b7166ff8d846665fce6d5779506032a08e
cesytar <cesytarr@gmail.com>
2025-09-09 22:39:12 +0200

    chore: agregar node_modules y config/keys.js al .gitignore

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
- MONGO_URI: 'mongodb+srv://cesytarr:CECILIACLUSTERbbk@clusterbbk.oepyqdj.mongodb.net/redSocial?retryWrites=true&w=majority&appName=ClusterBBK',
- jwt_secret: 'redsocial',
-}
